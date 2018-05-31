e10ee13d76197262df31509682225bde446be403
LW-Ho <leoleo41306@gmail.com>
2018-05-31 10:55:32 +0800

    tableView Can show the values. then we calculate it.

--- HouseMeasure/resultTableViewController.swift
+++ HouseMeasure/resultTableViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class resultTableViewController: UITableViewController {
 
-    var getResultDictionaries: Dictionary = [String:[Float]]()
+    var getResultDictionaries = [String:[Float]]()
+    var getTitleSection = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
+        getTitleSection = [String](getResultDictionaries.keys)
         //print(getResultDictionaries)
         //print(getResultDictionaries.count)
         // Uncomment the following line to preserve selection between presentations
@@ -33,62 +35,39 @@ class resultTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        //return getTitleSection.count
+        return getTitleSection.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-    
-//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        //return getResultDictionaries[section]
-//    }
+        let valuesKey = getTitleSection[section]
+        guard let titleValues = getResultDictionaries[valuesKey] else {
+            return 0
+        }
 
-    /*
+        return titleValues.count
+        //return 0
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return getTitleSection[section]
+    }
+
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         // Configure the cell...
+        let valuesKey = getTitleSection[indexPath.section]
+        if let titleValues = getResultDictionaries[valuesKey] {
+            cell.textLabel?.text = String(titleValues[indexPath.row])
+        }
 
         return cell
     }
-    */
+ 
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -113,5 +92,6 @@ extension resultTableViewController {
     
     @IBAction func debugBtn(_ sender: Any) {
         print(getResultDictionaries)
+        print(getTitleSection.count)
     }
 }
